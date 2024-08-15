2dd2edcce3471961f73479e9c1d65565c8c3d7b3
unknown <sanjoykundu187@gmail.com>
2024-08-15 16:59:20 +0600

    variable scope started

--- Introduction/index.php
+++ Introduction/index.php
@@ -19,7 +19,7 @@
     box-shadow: 1px 1px 1px 0px;
     }
     main{
-        height:500px;
+        height:100%;
         width:100%;
         background-color:#f7d4d4;
     }
@@ -37,6 +37,83 @@
         <hr>
         <hr>
         <h2>Variable</h2>
+        <?php 
+        $firstName = "Sanjoy";                     // variable  type string
+        $lastName  = "Kundu";                      // variable  type string
+        $address   = "Madaripur";                 // variable  type string
+        $email     = "sanjoykundu187@gmail.com";
+        $sanjoysInfo = "My name is $firstName $lastName. Im form $address. My email address is $email";
+        
+        echo $email."<br/>"; 
+        echo $sanjoysInfo;
+        ?>
+        <h3>Types of variable</h3>
+        <ol style="margin-left:13%">
+            <li>String</li>
+            <li>Integer</li>
+            <li>Float</li>
+            <li>Array</li>
+            <li>Object</li>
+            <li>Null</li>
+            <li>Boolean</li>
+        </ol>
+        <br>
+        <h3>Check php data type uisng var_dump() function</h3>
+        <?php
+        $num1 = 10;       //integer
+        $num2 = 10.55;   //float
+        $num3 = true;   //boolean
+        $num4 = ["volvo", "sacnia", "hino"];  //array
+        $num5 = "Im form Bangladesh";   //string
+        $num6 = NULL;
+       ?>
+       <ul>
+        <li>Checking varable type 
+            <ol>
+                <li>Fist Number is <?= var_dump($num1)?></li>
+                <li>Second Number is <?= var_dump($num2)?></li>
+                <li>Third Number is <?= var_dump($num3)?></li>
+                <li>Fourth Number is <?= var_dump($num4)?></li>
+                <li>Five Number is <?= var_dump($num5)?></li>
+                <li>Six Number is <?= var_dump($num6)?></li>
+            </ol>
+        </li>
+
+        <li>Assign Multiple Value
+            <ol>
+                <li><?= $x = $y = $z = "ABCDEF"?></li>
+                <li><?php echo $x ?></li>
+                <li><?php echo $y ?></li>
+                <li><?php echo $z ?></li>
+            </ol>
+        </li>
+       </ul>
+
+    <br><br><br><br><br>
+        
+
+
+
+<h3>Variable Scope</h3>
+<h5>There are 3 kinds of variable scope: </h5>
+<ol type="i" style="padding-left:30px;">
+    <li>Local</li>
+    <li>Global</li>
+    <li>Static</li>
+</ol>
+
+
+
+
+
+
+
+
+
+
+
+
+
     </main>
     <footer>we are learning php</footer>
 </body>
